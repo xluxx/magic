17e16b85b6baa4c8ef3db345f44de7c45f7023a7
xluxx <125575752+xluxx@users.noreply.github.com>
2025-01-30 20:02:17 +0300

    Update magic.py

--- magic.py
+++ magic.py
@@ -46,39 +46,21 @@ h2 = House('ЖК Акация', 20)
 
 print(h1)
 print(h2)
-
-
-print(h1 == h2) # __eq__
-
-
-
-h1 = h1 + 10 # __add__
-
-print(h1)
-
 print(h1 == h2)
 
+h1 = h1 + 10 
+print(h1)
+print(h1 == h2)
 
-
-h1 += 10 # __iadd__
-
+h1 += 10 
 print(h1)
 
-
-
-h2 = 10 + h2 # __radd__
-
+h2 = 10 + h2 
 print(h2)
 
-
-
-print(h1 > h2) # __gt__
-
-print(h1 >= h2) # __ge__
-
-print(h1 < h2) # __lt__
-
-print(h1 <= h2) # __le__
-
-print(h1 != h2) # __ne__
+print(h1 > h2) 
+print(h1 >= h2) 
+print(h1 < h2) 
+print(h1 <= h2) 
+print(h1 != h2) 
 
